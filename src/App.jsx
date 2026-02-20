b47dca4ea41313288c1561c50ec940378ec4fff1
Raphael Reis <reis.rrsc@outlook.com>
2026-02-20 10:58:15 -0300

    componente em forma de classe

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,36 @@
-const App = () => {
-  const arr = [1, 2, 3, 4, 5, 6];
-  return (
-    <div className="App">
-      <h1>Teste</h1>
-      {arr.map((item) => {
-        return <p>{item}</p>;
-      })}
-    </div>
-  );
-};
+// const App = () => {
+//   const arr = [1, 2, 3, 4, 5, 6];
+//   return (
+//     <div className="App">
+//       <h1>Teste</h1>
+//       {arr.map((item) => {
+//         return <p>{item}</p>;
+//       })}
+//     </div>
+//   );
+// };
+
+// export default App;
+
+import React, { Component } from "react";
+
+class App extends Component {
+  state = {
+    usuarios: ["A", "B", "C"],
+  };
+
+  render() {
+    const { usuarios } = this.state;
+
+    return (
+      <div className="App">
+        <h1>Teste</h1>
+        {usuarios.map((item) => (
+          <p>{item}</p>
+        ))}
+      </div>
+    );
+  }
+}
 
 export default App;
