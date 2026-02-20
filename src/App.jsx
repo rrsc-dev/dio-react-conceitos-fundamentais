4072fa95dfdf90de0057dcec86e5ae0a0c57cab3
Raphael Reis <reis.rrsc@outlook.com>
2026-02-20 10:55:14 -0300

    Estudo jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,11 @@
 const App = () => {
+  const arr = [1, 2, 3, 4, 5, 6];
   return (
     <div className="App">
       <h1>Teste</h1>
+      {arr.map((item) => {
+        return <p>{item}</p>;
+      })}
     </div>
   );
 };
